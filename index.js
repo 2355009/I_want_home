c73c13e510d1c1ac09ec6df0a0ab18e298dfdfa1
kbu2355009 <anyoungjin0106@kbu.ac.kr>
2023-04-11 16:27:04 +0900

    수정

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-img=document.querySelectorAll('img');
-img.array.forEach(element => {
-    element.src='https://item.kakaocdn.net/do/7eedd6cf5c1751f8bacfbbce8e8d48d5ce9463e040a07a9462a54df43e1d73f1'
-});
+setInterval(() => {
+    imgs = document.querySelectorAll('img');
+    imgs.forEach((a) => {
+        a.src = 'https://item.kakaocdn.net/do/7eedd6cf5c1751f8bacfbbce8e8d48d5ce9463e040a07a9462a54df43e1d73f1'
+    })
+},0.001)
