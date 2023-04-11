8d7c3d496fafca245eaf6726e7198eca825609cf
kbu2355009 <anyoungjin0106@kbu.ac.kr>
2023-04-11 16:27:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 setInterval(() => {
     imgs = document.querySelectorAll('img');
     imgs.forEach((a) => {
-        a.src = 'https://item.kakaocdn.net/do/7eedd6cf5c1751f8bacfbbce8e8d48d5ce9463e040a07a9462a54df43e1d73f1'
+        a.src = 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb1Yb3S%2FbtrmZJCcg0A%2FFq0qKtADkZRZtl9MomkiM1%2Fimg.jpg'
     })
 },0.001)
