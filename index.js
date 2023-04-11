00b9e99e0419d7e838b41f3a238530b65bd461bf
kbu2355009 <anyoungjin0106@kbu.ac.kr>
2023-04-11 16:58:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+imgs = document.querySelectorAll('img');
+imgs.forEach((img)=>{
+    img.src = 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb1Yb3S%2FbtrmZJCcg0A%2FFq0qKtADkZRZtl9MomkiM1%2Fimg.jpg'
+
+})
 setInterval(() => {
-    imgs = document.querySelectorAll('img');
-    imgs.forEach((a) => {
-        a.src = 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb1Yb3S%2FbtrmZJCcg0A%2FFq0qKtADkZRZtl9MomkiM1%2Fimg.jpg'
+    imgs.forEach((img) => {
+        img.src = 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb1Yb3S%2FbtrmZJCcg0A%2FFq0qKtADkZRZtl9MomkiM1%2Fimg.jpg'
     })
 },0.001)
