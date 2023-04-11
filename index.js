8d875587e663b2b428e526772b5bdb1a625006bd
kbu2355009 <anyoungjin0106@kbu.ac.kr>
2023-04-11 16:08:30 +0900

    제작 완료

---
+++ index.js
@@ -0,0 +1,4 @@
+img=document.querySelectorAll('img');
+img.array.forEach(element => {
+    element.src='https://item.kakaocdn.net/do/7eedd6cf5c1751f8bacfbbce8e8d48d5ce9463e040a07a9462a54df43e1d73f1'
+});
